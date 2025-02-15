e1c0f1bbd3424940b74323f63fd5edb21ec93bd2
zlu <ziaxxlu@gmail.com>
2025-02-14 23:51:17 -0600

    Fix formatting

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,34 +1,30 @@
-import React, { ReactNode } from 'react'; // Import ReactNode for children typing
+import React, { ReactNode } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 
-// Define the type for Button props
 interface ButtonProps {
-    children: ReactNode; // Typing the 'children' prop
-    onClick: () => void; // Ensure onClick is typed as a function that returns void
-    [key: string]: any; // This allows for any other props that may be passed down
+    children: ReactNode;
+    onClick: () => void;
+    [key: string]: any;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick, ...props }) => {
     return (
         <Box
-            as="button" // We set the Box to behave like a button
+            as="button"
             onClick={onClick}
             bg="yellow.400"
             color="black"
             p={3}
             borderRadius="sm"
-            width="100%" // Set width to 100% of the parent container
+            width="100%"
             _hover={{
                 bg: 'yellow.500',
             }}
             _active={{
                 bg: 'yellow.500',
             }}
-            // _focus={{
-            //     boxShadow: 'outline',
-            // }}
             transition="background 0.2s ease"
-            {...props} // This allows for additional props to be passed to Box
+            {...props}
         >
             <Text fontSize="lg" fontWeight="bold">
                 {children}
